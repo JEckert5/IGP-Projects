f0caff5772577ef23c87d74ab3747bfadcc75b62
Jens Eckert <jeckert5@kent.edu>
2023-09-08 17:05:23 -0400

    I need more boullets

--- assignment1_jenseckert/assignment1_win/main.cpp
+++ assignment1_jenseckert/assignment1_win/main.cpp
@@ -8,14 +8,14 @@
 
 using namespace sf;
 
-int width = 800, height = 600;
+int width = 940, height = 720;
 
 static float deltaTime = 0.0f;
 
 // Ripped from the asteroid demo.
 class Animation {
 public:
-    Animation() { }
+    Animation() = default;
 
     Animation(Texture& t, int x, int y, int w, int h, int count, float speed) {
         frame = 0;
@@ -36,6 +36,10 @@ public:
         if (n > 0) sprite.setTextureRect(frames[int(frame)]);
     }
 
+    Sprite& getSprite() {
+        return sprite;
+    }
+ 
 private:
     Sprite sprite;
     std::vector<IntRect> frames;
@@ -43,10 +47,38 @@ private:
     float frame, fps;
 };
 
-class Video {
+class Entity {
 public:
+    Entity() = default;
+
+    Entity(std::list<Entity*>* ents): globlEntities(ents) {}
+
+    virtual void tick() { };
+
+    virtual void draw(RenderWindow& window) { }
+
+    float getSpeed() const { return speed; }
+
+    const void setSpeed(float x) { speed = x; }
+
+    std::string name;
+
+protected:
+    Animation anim;
+    float speed;
+    Vector2<float> pos;
+    std::list<Entity*>* globlEntities = nullptr;
+};
+
+class Video: public Entity {
+public:
+    Video(std::list<Entity*>* ent){//: globlEntities(ent) {
+        globlEntities = ent;
+        name = "Video";
+    }
+
     // relies on files having format 'output-XXXX.png' naming format.
-    Video(const std::string& frameFolder, int numFrames, float fps, const std::string& audioPath) { 
+    void install(const std::string& frameFolder, int numFrames, float fps, const std::string& audioPath) {
         this->fps = 1 / fps;
         audio.openFromFile(audioPath);
         frame = 0;
@@ -67,8 +99,6 @@ public:
             Texture t;
             t.loadFromFile(frameFolder + file + std::to_string(i) + ".png");
 
-            // std::cout << "img " << i << std::endl;
-
             frames.emplace_back(t);
         }
 
@@ -76,7 +106,7 @@ public:
         accumulator = 0.0f;
     }
 
-    void update() {
+    void tick() override {
         accumulator += deltaTime;
 
         if (accumulator >= fps) {
@@ -96,7 +126,7 @@ public:
         return playing;
     }
 
-    void draw(RenderWindow& window) {
+    void draw(RenderWindow& window) override {
         window.draw(sprite);
     }
 private:
@@ -108,69 +138,100 @@ private:
     float fps, accumulator;
 };
 
-class Entity {
-public:
-    Entity() { }
-
-    virtual void tick() {};
-
-    virtual void draw(RenderWindow& window) {
-        
-    }
-
-    float getSpeed() const {
-        return speed;
-    }
-
-    const void setSpeed(float x) {
-        speed = x;
-    }
-
-protected:
-    Animation anim;
-    float speed;
-};
-
 class Enemy: public Entity {
+public:
     void draw(RenderWindow& window) override {
 
     }
+};
+
+class Bullet: public Entity {
+public:
+    Bullet(float v, Vector2<float> dir, float p, std::list<Entity*>* end): velocity(v), direction(dir), power(p) {
+        globlEntities = end;
+        bt.loadFromFile("images/bullet.gif");
+        anim.getSprite().setTexture(bt);
+        name = "Bullet";
+        pos = dir;
+    }
+
+    ~Bullet() {
+        // std::cout << "FORTNITE!!\n" << std::endl;
+        globlEntities = nullptr;
+    }
+
+    void tick() override {
+        pos.y += -direction.y * velocity * deltaTime;
+        anim.getSprite().setPosition(pos);
+
+        if (pos.y < 0) {
+            // std::cout << "Hello.\n" << std::endl;
+            markedForNegation = true;
+            // std::cout << "Hellllsdfsdfsdf\n" << std::flush;
+        }
+    }
+
+    void draw(RenderWindow& window) override {
+        window.draw(anim.getSprite());
+    }
+
+    bool isMarkedForNegation() const {
+        return markedForNegation;
+    }
+
+private:
+    float velocity;
+    Vector2<float> direction;
+    float power;
+    Texture bt;
+    bool markedForNegation = false;
 };
 
 class Player: public Entity {
 public:
-    Player(Texture& t) {
-        y = height - t.getSize().y;
+    Player(Texture& t, std::list<Entity*>* ent) {
+        globlEntities = ent;
+        pos.y = height - t.getSize().y;
 
-        sprite.setTexture(t);
+        anim.getSprite().setTexture(t);
+        anim.getSprite().setScale(0.6f, 0.6f);
 
-        x = width / 4;
+        pos.x = width / 4;
 
-        sprite.setPosition(x, y);
+        anim.getSprite().setPosition(pos);
+
+        name = "Player";
     }
 
     void tick() override {
         if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
-            x -= speed * deltaTime;
+            pos.x -= speed * deltaTime;
         if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
-            x += speed * deltaTime;
+            pos.x += speed * deltaTime;
         if (Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up))
-            y -= speed * deltaTime;
+            pos.y -= speed * deltaTime;
         if (Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))
-            y += speed * deltaTime;
+            pos.y += speed * deltaTime;
 
-        sprite.setPosition(x, y);
+        // Instantiate new bullet
+        if (Keyboard::isKeyPressed(Keyboard::Space) && fireTimer < 0) {
+            globlEntities->push_back(new Bullet(1, Vector2<float>(pos), 10, globlEntities));
+            fireTimer = 20;
+            std::cout << "Space" << std::endl;
+        }
+
+        std::cout << "Gug" << std::endl;
+        anim.getSprite().setPosition(pos);
+        fireTimer = fireTimer > -0.1f ? fireTimer - 0.1f : -0.1f;
     }
 
     void draw(RenderWindow& window) override {
-        window.draw(sprite);
+        window.draw(anim.getSprite());
     }
 
-    Sprite sprite;
-
 private:
-    float x, y;
     int health = 100;
+    float fireTimer = -0.1f;
 };
 
 int main() {
@@ -182,45 +243,50 @@ int main() {
 
     std::list<Entity*> entities;
 
-    Texture pt, et, bt;
+    Texture pt;
     pt.loadFromFile("images/player.png");
 
-    Player* player = new Player(pt);
+    Player* player = new Player(pt, &entities);
     player->setSpeed(500);
 
     entities.push_back(player);
 
     Clock clock;
-
-    Video touhou("images/touhou", 6572, 33.0003f, "audio/badapple.wav");
+    Video* touhou = new Video(&entities);
 
     while (window.isOpen()) {
         deltaTime = clock.restart().asSeconds();
 
         Event e;
         while (window.pollEvent(e)) {
-            if (e.type == Event::Closed)
+            if (e.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
                 window.close();
 
-            if (Keyboard::isKeyPressed(Keyboard::F)) {
-                touhou.start();
+            if (Keyboard::isKeyPressed(Keyboard::F) && !touhou->isPlaying()) {
+                touhou->install("images/touhou", 6572, 30.0003f, "audio/badapple.wav");
+                entities.push_back(touhou);
+                touhou->start();
             }
         }
 
-        for (auto& e: entities) {
-            e->tick();
+        for (auto it = entities.begin(); it != entities.end(); it++) {
+            (*it)->tick();
+
+            if ((*it)->name == "Bullet") {
+                if (static_cast<Bullet*>(*it)->isMarkedForNegation()) {
+                    auto e = (*it);
+                    it = entities.erase(it);
+                    delete e;
+                }
+            }
         }
+
+        std::cout << "Help" << std::endl;
 
         window.clear();
-        
-        for (auto& e: entities) {
-            e->draw(window);
-        }
 
-        if (touhou.isPlaying()) {
-            touhou.update();
-            touhou.draw(window);
-        }
+        for (auto e: entities)
+            e->draw(window);
 
         window.display();
     }
