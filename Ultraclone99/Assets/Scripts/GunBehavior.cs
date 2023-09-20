176346c3ae55d8e6e72766c33f9d6d08faa8c04c
Jens Eckert <jeckert5@kent.edu>
2023-09-20 11:44:02 -0400

    Add 2D game assignment

--- Ultraclone99/Assets/Scripts/GunBehavior.cs
+++ Ultraclone99/Assets/Scripts/GunBehavior.cs
@@ -27,15 +27,18 @@ public class GunBehavior : MonoBehaviour {
     private void OnFire(InputAction.CallbackContext ctx) {
         // RaycastHit hit;
         
+        Debug.Log(transform.forward);
         Debug.Log(transform.TransformDirection(transform.forward));
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hit)) {
-            Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward) * hit.distance, Color.green, 3);
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green, 3);
             Debug.Log("hit");
         } else {
-            Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward) * 1000, Color.red, 3);
+            Debug.DrawRay(transform.position, transform.forward * 1000, Color.red, 3);
             Debug.Log("no hit");
         }
+        
+        // Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward) * 1000, Color.blue, 3);
     }
 
     private void OnEnable() {
